0ba836e7827ad977810ff41b53151ca7c3b6acb7
timoh <timoh6@gmail.com>
2017-12-07 09:50:59 +0200

    Fix PHPUnit backward compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,12 @@
 chdir(__DIR__);
 error_reporting(E_ALL | E_STRICT);
 
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase'))
+{
+	class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
+
 require_once '../library/GenPhrase/Loader.php';
 $loader = new GenPhrase\Loader('GenPhrase');
 $loader->register();
